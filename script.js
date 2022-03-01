183810d57171cc970b04bba833d54e56a97f1d3e
Anton Rastvorov <antonrostvorov.exceedteam@gmail.com>
2022-03-01 13:26:06 +0300

    doubleclick

--- script.js
+++ script.js
@@ -4,208 +4,354 @@ let inputWhere = null;
 let inputHow = null;
 
 window.onload = () => {
-	inputWhere = document.getElementById("input-where");
-	inputHow = document.getElementById("input-how");
-	inputWhere.addEventListener("change", updateWhereText);
-	inputHow.addEventListener("change", updateHowText);
-	render();
+  inputWhere = document.getElementById("input-where");
+  inputHow = document.getElementById("input-how");
+  inputWhere.addEventListener("change", updateWhereText);
+  inputHow.addEventListener("change", updateHowText);
+  render();
 };
 
-const dataPost = async (method = "", url = "", data = {}) => {
-	const response = await fetch(url, {
-		method,
-		headers: {
-			"Content-Type": "application/json; charset=utf-8",
-			"Access-Control-Allow-Origin": "*",
-		},
-		body: JSON.stringify(data),
-	});
-	return response;
+const postPachData = async (method = "", url = "", data = {}) => {
+  const response = await fetch(url, {
+    method,
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(data),
+  });
+  return response;
 };
 
-const updateWhereText = (event) => {
-	whereText = event.target.value;
+const updateWhereText = (event, a) => {
+  whereText = event.target.value;
 };
 
 const updateHowText = (event) => {
-	if (+event.target.value != 0) {
-		howNumber = event.target.value;
-	} else {
-		inputHow.value = "";
-		alert("Введите числовое значение больше 0!");
-	}
+  if (+event.target.value && +event.target.value > 0) {
+    howNumber = event.target.value;
+  } else {
+    inputHow.value = "";
+    alert("Введите верное числовое значение!");
+  }
 };
 
 const onButtonAddClick = async () => {
-	if (whereText !== null && howNumber !== null) {
-		dataPost("POST", "http://localhost:8000/newTicket", {
-			text: whereText,
-			cost: howNumber,
-		})
-			.then((data) => {
-				render();
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	} else alert("Введите данные!");
-	whereText = null;
-	howNumber = null;
-	inputHow.value = "";
-	inputWhere.value = "";
+  if (whereText.trim().length !== 0 && howNumber !== null) {
+    postPachData("POST", "http://localhost:8000/newReceipt", {
+      text: whereText,
+      cost: howNumber,
+    })
+      .then((data) => {
+        render();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  } else alert("Введите данные!");
+  whereText = null;
+  howNumber = null;
+  inputHow.value = "";
+  inputWhere.value = "";
 };
 
-const onClickEdit = async (index, ticketArray) => {
-	const { _id, text, cost, date } = ticketArray[index];
-	let editText = text;
-	let editCost = cost;
-	let editDate = date;
+const onClickEdit = async (index, receiptArr) => {
+  const { _id, text, cost, date } = receiptArr[index];
+  let editText = text;
+  let editCost = cost;
+  let editDate = date;
 
-	const container = document.getElementById(`ticket=${_id}`);
-	container.className = "ticket-block edit-ticket-block";
-	while (container.firstChild) {
-		container.removeChild(container.firstChild);
-	}
+  const container = document.getElementById(`receipt=${_id}`);
+  container.className = "receipt-block edit-receipt-block";
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 
-	const inputText = document.createElement("input");
-	inputText.value = text;
-	inputText.addEventListener("change", (event) => {
-		editText = event.target.value;
-	});
+  const inputText = document.createElement("input");
+  inputText.value = text;
+  inputText.addEventListener("change", (event) => {
+    editText = event.target.value;
+  });
 
-	const inputDate = document.createElement("input");
-	inputDate.value = editDate.slice(0, 10);
-	inputDate.type = "date";
+  const inputCost = document.createElement("input");
+  inputCost.value = cost;
+  inputCost.addEventListener("change", (Event) => {
+    editCost = Event.target.value;
+  });
 
-	const inputCost = document.createElement("input");
-	inputCost.value = cost;
-	inputCost.addEventListener("change", (event) => {
-		editCost = event.target.value;
-	});
+  const inputDate = document.createElement("input");
+  inputDate.value = editDate.slice(0, 10);
+  inputDate.type = "date";
+  let now = new Date();
+  inputDate.min = `${now.getFullYear()}-${now.getMonth() - 1}-${now.getDate()}`;
+  inputDate.max = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`;
 
-	inputDate.addEventListener("change", (event) => {
-		editDate = event.target.value;
-	});
+  inputDate.addEventListener("change", (Event) => {
+    editDate = Event.target.value;
+  });
 
-	const editSendButton = document.createElement("button");
-	editSendButton.innerText = "Edit";
-	editSendButton.onclick = () => {
-		if (
-			editText.trim().length !== 0 &&
-			editCost !== 0 &&
-			editDate.trim().length !== 0
-		) {
-			if (+editCost) {
-				dataPost("PATCH", "http://localhost:8000/updateTicket", {
-					id: _id,
-					text: editText,
-					cost: editCost,
-					date: editDate,
-				}).then((data) => {
-					render();
-				})
-					.catch((err) => {
-						console.log(err);
-					});
-			} else alert("Введите числовое значение!");
-		} else alert("Введите данные");
-	};
+  const editSendButton = document.createElement("button");
+  editSendButton.innerText = "Edit";
 
-	const cancelButton = document.createElement("button");
-	cancelButton.innerText = "Cancel";
-	cancelButton.onclick = () => render();
+  editSendButton.onclick = () => {
+    if (
+      editText.trim().length !== 0 &&
+      editCost !== 0 &&
+      editDate.trim().length !== 0
+    ) {
+      if (+editCost && +editCost > 0) {
+        postPachData("PATCH", "http://localhost:8000/updateTicket", {
+          id: _id,
+          text: editText,
+          cost: editCost,
+          date: editDate,
+        })
+          .then((data) => {
+            render();
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      } else alert("Введите числовое значение!");
+    } else alert("Введите данные");
+  };
+  const cancelButton = document.createElement("button");
+  cancelButton.innerText = "Cancel";
 
-	container.appendChild(inputText);
-	container.appendChild(inputDate);
-	container.appendChild(inputCost);
-	container.appendChild(editSendButton);
-	container.appendChild(cancelButton);
+  cancelButton.onclick = () => render();
+
+  container.appendChild(inputText);
+  container.appendChild(inputCost);
+  container.appendChild(inputDate);
+  container.appendChild(editSendButton);
+  container.appendChild(cancelButton);
 };
 
 const onClickDelete = async (id) => {
-	const resp = await fetch(`http://localhost:8000/deleteTicket?id=${id}`, {
-		method: "DELETE",
-		headers: {
-			"Content-Type": "application/json; charset=utf-8",
-			"Access-Control-Allow-Origin": "*",
-		},
-	});
-	render();
+  const resp = await fetch(`http://localhost:8000/deleteTicket?id=${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+
+  render();
+};
+
+const ondblclickBlock = (id, data, type) => {
+  let divBoxData = document.getElementById(`${type}=${id}`);
+  let divBox = document.getElementById(`div_${type}=${id}`);
+  while (divBox.firstChild) {
+    divBox.removeChild(divBox.firstChild);
+  }
+  switch (type) {
+    case "text":
+      let editText = data;
+      const inputText = document.createElement("input");
+      inputText.value = editText;
+      inputText.id = `${type}=${id}`;
+
+      const handleClick1 = (Event) => {
+        if (Event.target.id != inputText.id) {
+          render();
+          document.removeEventListener("click", handleClick1);
+        }
+      }
+
+      document.addEventListener("click", handleClick1);
+
+      inputText.addEventListener("blur", (Event) => {
+        editText = Event.target.value;
+        if (editText.trim().length !== 0) {
+          postPachData("PATCH", `http://localhost:8000/updateTicket`, {
+            id,
+            text: editText,
+          })
+            .then((data) => {
+              render();
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        } else {
+          inputText.value = data;
+          alert("Введите текст!");
+        }
+      });
+      divBox.appendChild(inputText);
+
+      break;
+
+    case "cost":
+      let editCost = data;
+      const inputCost = document.createElement("input");
+      inputCost.value = editCost;
+      inputCost.id = `${type}=${id}`;
+
+      const handleClick2 = (Event) => {
+        if (Event.target.id != inputCost.id) {
+          render();
+          document.removeEventListener("click", handleClick2);
+        }
+      }
+
+      document.addEventListener("click", handleClick2);
+
+      inputCost.addEventListener("blur", (Event) => {
+        editCost = Event.target.value;
+        if (+editCost && editCost > 0) {
+          postPachData("PATCH", "http://localhost:8000/updateReceipt", {
+            id,
+            cost: editCost,
+          })
+            .then((data) => {
+              render();
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        } else {
+          inputCost.value = data;
+          alert("Ведите верное числовое значение!");
+        }
+      });
+      divBox.appendChild(inputCost);
+      break;
+
+    case "date":
+      let editDate = `${data.slice(8, 10)}-${data.slice(5, 7)}-${data.slice(0,4)}`;
+
+      const inputDate = document.createElement("input");
+      inputDate.type = "date";
+      let now = new Date();
+      inputDate.min = `${now.getFullYear()}-${
+        now.getMonth() - 2
+      }-${now.getDate()}`;
+      inputDate.max = `${now.getFullYear()}-${
+        now.getMonth() + 1
+      }-${now.getDate()}`;
+      inputDate.value = data.slice(0, 10);
+      inputDate.id = `${type}=${id}`;
+
+      const handleClick3 = (event) => {
+        if (event.target.id != inputDate.id) {
+          render();
+          document.removeEventListener("click", handleClick3);
+        }
+      }
+
+      document.addEventListener("click", handleClick3);
+
+      inputDate.addEventListener("blur", (Event) => {
+        editDate = Event.target.value;
+        if (editDate.trim().length !== 0) {
+          postPachData("PATCH", "http://localhost:8000/updateTicket", {
+            id,
+            date: editDate,
+          })
+            .then((data) => {
+              render();
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        } else {
+          inputText.value = data;
+          alert("Введите правильную дату!");
+        }
+      });
+      divBox.appendChild(inputDate);
+      break;
+  }
 };
 
 const render = async () => {
-	const response = await fetch("http://localhost:8000/allTickets", {
-		method: "GET",
-	});
+  const response = await fetch("http://localhost:8000/allTickets", {
+    method: "GET",
+  });
 
-	const allUserSpending = await fetch(
-		"http://localhost:8000/allUserSpending",
-		{
-			method: "GET",
-		}
-	);
-	const result = await response.json();
-	const ticketArray = result.data;
+  const allUserSpending = await fetch(
+    "http://localhost:8000/allUserSpending",
+    {
+      method: "GET",
+    }
+  );
+  const result = await response.json();
+  const receiptArr = result.data;
 
-	const resultUserSpending = await allUserSpending.json();
+  const resultUserSpending = await allUserSpending.json();
 
-	const contentBlock = document.getElementById("content-container");
-	const totalWaste = document.getElementById("total-waste");
+  const UserSpending = resultUserSpending.data;
 
-	totalWaste.innerText = `Итого: ${resultUserSpending.total} руб.`;
-	while (contentBlock.firstChild) {
-		contentBlock.removeChild(contentBlock.firstChild);
-	}
+  const contentBlock = document.getElementById("content-container");
+  const totalWaste = document.getElementById("total-waste");
 
-	ticketArray.map((item, index) => {
-		const { _id, text, cost, date } = item;
-		const container = document.createElement("div");
-		container.id = `ticket=${_id}`;
-		container.className = "ticket-block";
+  totalWaste.innerText = `Итого: ${resultUserSpending.total} р.`;
+  while (contentBlock.firstChild) {
+    contentBlock.removeChild(contentBlock.firstChild);
+  }
 
-		const ticketDelete = document.createElement("img");
-		ticketDelete.src = "delete.png";
-		ticketDelete.onclick = () => onClickDelete(_id);
+  receiptArr.map((item, index) => {
+    const { _id, text, cost, date } = item;
+    const container = document.createElement("div");
+    container.id = `receipt=${_id}`;
+    container.className = "receipt-block";
 
-		const divInfoBlock = document.createElement("div");
-		divInfoBlock.className = "div-info-block";
+    const divImgBlock = document.createElement("div");
+    divImgBlock.className = "div-img-block";
+    const receiptEdit = document.createElement("img");
+    receiptEdit.src =
+      "https://img.icons8.com/material-outlined/24/000000/pencil--v1.png";
 
-		const divTextBlock = document.createElement("div");
-		divTextBlock.className = "div-text-block";
-		const textBlock = document.createElement("p");
-		textBlock.ondblclick = () => onClickEdit(index, ticketArray)
-		textBlock.innerText = `${index + 1}) ${text}`;
-		divTextBlock.appendChild(textBlock);
-		divInfoBlock.appendChild(divTextBlock);
+    receiptEdit.onclick = () => onClickEdit(index, receiptArr);
+    divImgBlock.appendChild(receiptEdit);
+    container.appendChild(divImgBlock);
 
-		const divCostBlock = document.createElement("div");
-		divCostBlock.className = "div-text-block";
-		const costBlock = document.createElement("p");
-		costBlock.ondblclick = () => onClickEdit(index, ticketArray)
-		costBlock.innerText = `${cost} руб.`;
+    const receiptDelete = document.createElement("img");
+    receiptDelete.src =
+      "https://img.icons8.com/material-outlined/24/000000/delete-sign.png";
 
-		const divDateBlock = document.createElement("div");
-		divDateBlock.className = "div-date-block";
-		const dateBlock = document.createElement("p");
-		dateBlock.innerText = `${date.slice(8, 10)}-${date.slice(5, 7)}-${date.slice(0, 4)}`;
-		dateBlock.ondblclick = () => onClickEdit(index, ticketArray)
+    receiptDelete.onclick = () => onClickDelete(_id);
+    divImgBlock.appendChild(receiptDelete);
+    container.appendChild(divImgBlock);
 
-		const divImgBlock = document.createElement("div");
-		divImgBlock.className = "div-img-block";
+    const divInfoBlock = document.createElement("div");
+    divInfoBlock.className = "div-info-block";
 
-		const ticketEdit = document.createElement("img");
-		ticketEdit.src = "edit.png";
-		ticketEdit.onclick = () => onClickEdit(index, ticketArray);
+    const divTextBlock = document.createElement("div");
+    divTextBlock.className = "div-text-block";
+    const textBlock = document.createElement("p");
+    textBlock.innerText = text;
+    divTextBlock.id = `div_text=${_id}`;
+    textBlock.id = `text=${_id}`;
+    textBlock.ondblclick = () => ondblclickBlock(_id, text, "text");
 
-		divImgBlock.appendChild(ticketEdit);
-		container.appendChild(divImgBlock);
-		divImgBlock.appendChild(ticketDelete);
-		divDateBlock.appendChild(dateBlock);
-		divInfoBlock.appendChild(divDateBlock);
-		divCostBlock.appendChild(costBlock);
-		divInfoBlock.appendChild(divCostBlock);
-		divInfoBlock.appendChild(divImgBlock)
-		container.appendChild(divInfoBlock);
-		contentBlock.appendChild(container);
-	});
+    divTextBlock.appendChild(textBlock);
+    divInfoBlock.appendChild(divTextBlock);
+
+    const divCostBlock = document.createElement("div");
+    divCostBlock.className = "div-cost-block";
+    const costBlock = document.createElement("p");
+    costBlock.innerText = `${cost} р.`;
+    divCostBlock.id = `div_cost=${_id}`;
+    costBlock.id = `cost=${_id}`;
+    costBlock.ondblclick = () => ondblclickBlock(_id, cost, "cost");
+
+    divCostBlock.appendChild(costBlock);
+    divInfoBlock.appendChild(divCostBlock);
+
+    const divDateBlock = document.createElement("div");
+    divDateBlock.className = "div-date-block";
+    const dateBlock = document.createElement("p");
+    dateBlock.innerText = `${date.slice(8, 10)}-${date.slice(5,7)}-${date.slice(0, 4)}`;
+    divDateBlock.id = `div_date=${_id}`;
+    dateBlock.id = `date=${_id}`;
+    dateBlock.ondblclick = () => ondblclickBlock(_id, date, "date");
+
+    divDateBlock.appendChild(dateBlock);
+    divInfoBlock.appendChild(divDateBlock);
+    container.appendChild(divInfoBlock);
+    contentBlock.appendChild(container);
+  });
 };
